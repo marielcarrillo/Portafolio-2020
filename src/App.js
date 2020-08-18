bff5c181a00dbc9ad3a2cb84f99040492ffecf3e
Mariel <marielcarrillo@MacBook-Pro-de-Mariel.local>
2020-08-18 01:31:44 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='triangle-about'></div>
+    <div className='web-view'>
+      <div className='triangle-dev'></div>
+      <div className='triangle-contact'></div>
+    </div>
+    <div className='skill-view'>
+      <div className='triangle-skills'></div>
+      <div className='triangle-skills-right'></div> 
+    </div>
+    </div>
+  );
+}
+
+export default App;
